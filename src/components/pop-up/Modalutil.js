1f40fe515619322999bb56abc8b107a6a1ac85f7
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-05 00:29:24 +0700

    i will fix someday

--- src/components/pop-up/Modalutil.js
+++ src/components/pop-up/Modalutil.js
@@ -3,5 +3,5 @@ import CustomModal from "./CustomModal";
 import { useState } from "react";
 export default function ModalUtil() {
     const [popUp, setPopUp] = useState(false);
-    return (_jsxs("div", { className: "flex flex-col justify-center items-center", children: [_jsx("button", { className: "px-6 py-2 bg-blue-400 rounded-md hover:to-blue-100 active:scale-95", onClick: () => setPopUp(!popUp), children: "Testing Pop Up" }), _jsx(CustomModal, { label: "Alert!!", message: "Did you wash your balls today?", confirm: "Done", cancel: "Never", show: popUp })] }));
+    return (_jsxs("div", { className: "flex flex-col justify-center items-center", children: [_jsx("button", { className: "px-6 py-2 bg-blue-400 rounded-md hover:to-blue-100 active:scale-95", onClick: () => setPopUp(!popUp), children: "Testing Pop Up" }), _jsx(CustomModal, { label: "Alert!!", message: "You are Testing very cool pop up ?", confirm: "Done", cancel: "Never", show: popUp })] }));
 }
