e0a39b22b192f110aa042ec07bc8d5a14bd2a84c
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-05 01:00:04 +0700

    fix someday

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
 		<main className="flex flex-col mx-auto   ">
 			<Header />
 			<div className="grid p-2 grid-cols-2 mt-[100px] sm:px-40 py-9  mx-auto items-center justify-items-center font-extralight text-black bg-white">
-				<img
+				{/* <img
 					className=""
 					src={
 						"https://depositphotos.com/photo/welcome-waiter-hotel-89823196.html"
@@ -13,7 +13,7 @@ const Home = () => {
 					alt="people"
 					width={200}
 					height={200}
-				/>
+				/> */}
 				<div className="sm:text-6xl text-2xl text-right hover:drop-shadow-xl">
 					I create this Website for practice and save some of my feature code
 					snippets that i mostly used
