1de2fe7b21dfbd076d46fabcb5224cdd41f32aba
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-05 01:19:51 +0700

    someday i will fix

--- src/pages/Home.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import Header from "../components/Header";
-const Home = () => {
-    return (_jsxs("main", { className: "flex flex-col mx-auto   ", children: [_jsx(Header, {}), _jsxs("div", { className: "grid p-2 grid-cols-2 mt-[100px] sm:px-40 py-9  mx-auto items-center justify-items-center font-extralight text-black  bg-gradient-to-r from-white to-slate-100", children: [_jsx("img", { className: "", src: "src/assets/people-white.jpg", alt: "people", width: 200, height: 200 }), _jsx("div", { className: "sm:text-6xl text-2xl text-right hover:drop-shadow-xl", children: "I create this Website for practice and save some of my feature code snippets that i mostly used" })] })] }));
-};
-export default Home;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,16 @@ const Home = () => {
 		<main className="flex flex-col mx-auto   ">
 			<Header />
 			<div className="grid p-2 grid-cols-2 mt-[100px] sm:px-40 py-9  mx-auto items-center justify-items-center font-extralight text-black bg-white">
-				{/* <img
-					className=""
+				<img
 					src={
-						"https://depositphotos.com/photo/welcome-waiter-hotel-89823196.html"
+						"https://st2.depositphotos.com/1552219/8982/i/950/depositphotos_89823196-stock-photo-welcome-waiter-hotel.jpg"
 					}
-					alt="people"
 					width={200}
 					height={200}
-				/> */}
+				/>
 				<div className="sm:text-6xl text-2xl text-right hover:drop-shadow-xl">
 					I create this Website for practice and save some of my feature code
-					snippets that i mostly used
+					snippets i mostly used...
 				</div>
 			</div>
 		</main>
